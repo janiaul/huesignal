25c2f56904920003be2459a3bfcfde81d8a84f40
janiaul <jani.aulavuo@gmail.com>
2026-03-14 18:54:24 +0200

    Rename icon variable for future use

--- huesignal/config.py
+++ huesignal/config.py
@@ -20,7 +20,7 @@ HUESIGNAL_HTML = EFFECTS_DIR / "HueSignal.html"
 CERT_FILE = CERTS_DIR / "localhost+1.pem"
 KEY_FILE = CERTS_DIR / "localhost+1-key.pem"
 CONFIG_FILE = BASE_DIR / "config.ini"
-TRAY_ICON = ASSETS_DIR / "tray.ico"
+ICON_FILE = ASSETS_DIR / "favicon.ico"
 
 FLASK_PORT = 5123
 WSS_URL = f"wss://127.0.0.1:{FLASK_PORT}/ws"
